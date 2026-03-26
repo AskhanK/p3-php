e7cf778da7b77e2f78841fb38327b71c13d2fa0e
AskhanK <9025194@student.zadkine.nl>
2026-03-26 15:28:57 +0100

    sf

---
+++ opdracht_1/shorthand.php
@@ -0,0 +1,22 @@
+<?php
+$naam = "Askhan";
+$leeftijd = 18;
+$taal = "HTML";
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Shorthand</title>
+</head>
+<body>
+
+    <h1>Mijn gegevens</h1>
+
+<p>Naam: <?= $naam ?></p>
+<p>Leeftijd: <?= $leeftijd ?></p>
+<p>Favoriete programmeertaal: <?= $taal ?></p>
+
+</body>
+</html>
