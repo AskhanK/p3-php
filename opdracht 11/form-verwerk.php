969c289704c42a83f741e8c7bd03cf3ab9950d65
AskhanK <9025194@student.zadkine.nl>
2026-03-30 15:25:07 +0200

    opdracht 11

---
+++ opdracht 11/form-verwerk.php
@@ -0,0 +1,12 @@
+<form action="verwerk.php" method="POST">
+    <label for="naam">Naam:</label>
+    <input type="text" id="naam" name="naam" required>
+    
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    
+    <label for="bericht">Bericht:</label>
+    <textarea id="bericht" name="bericht"></textarea>
+    
+    <input type="submit" value="Verzenden">
+</form>
